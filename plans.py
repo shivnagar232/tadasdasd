289d68033eb6597d65c2d5c629feb05f4f2063d5
shivnagar232 <154045191+shivnagar232@users.noreply.github.com>
2024-04-05 22:36:06 +0530

    Update plans.py

--- plans.py
+++ plans.py
@@ -12,6 +12,6 @@ async def plans_command(m):
     else:
         # Free user
         full_name = user.first_name + (f" {user.last_name}" if user.last_name else "")
-        reply_text = f"User ID: {user_id}\nName: {full_name}\n\n💠 Premium\n\n  ✓ Download Upto 2.0 GB\n  ✓ Task Limit: NO LIMIT\n  ✓ Time Gap: NO\n  ✓ No Anti-Spam Timer\n  ✓ Validity: 1 MONTH\n\n  Amount: 60 INR ₹\n\nBUY NOW FROM : @Mavisup_bot"
+        reply_text = f"User ID: {user_id}\nName: {full_name}\n\n💠 Premium(not applicable for now) \n\n  ✓ Download Upto 2.0 GB\n  ✓ Task Limit: NO LIMIT\n  ✓ Time Gap: NO\n  ✓ No Anti-Spam Timer\n  ✓ Validity: 1 MONTH\n\n  Amount: 100 INR ₹\n\nBUY NOW FROM : @team_spy_bot"
 
     await m.reply(reply_text, parse_mode="markdown")
